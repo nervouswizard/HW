6c8440db295836898a136145a81ded631af65723
nervouswizard <book4208@gmail.com>
2022-03-30 02:17:34 +0800

    finally

--- hw1.cpp
+++ hw1.cpp
@@ -1,16 +1,25 @@
 #include<bits/stdc++.h>
+#include <time.h>
 using namespace std;
 const int INF = 1e9+7;
-int *a, n, *heap, top=0;
+int a[1000000], n, heap[1000001], top, temp[1000000], t[1000000];
 int s[]{10,10000,50000,100000,200000,300000,400000,500000,600000,700000,800000,900000,1000000};
 timespec S, E;
-string test(int *t){
-	int *tt;
-	copy(t, t+n, tt);
-	sort(tt, tt+n);
+bool cmp(int a, int b){
+	return a<b;
+}
+void print(){
 	for (int i=0;i<n;i++){
-		if (tt[i]!=a[i]) return " BAD!";
+		cout << a[i] <<" ";
 	}
+	cout << "\n";
+}
+string test(){
+//	print();
+//	for (int i=0;i<n;i++) cout << t[i] << " ";
+//	cout << "\n";
+	for (int i=0;i<n;i++)
+		if (t[i]!=a[i]) return " BAD!";
 	return " NICE!";
 }
 int power(int a, int x){
@@ -20,12 +29,6 @@ int power(int a, int x){
 			ans=ans*a;
 	}
 	return ans;
-}
-void print(){
-	for (int i=0;i<n;i++){
-		cout << a[i] <<" ";
-	}
-	cout << "\n";
 }
 long double diff(timespec S, timespec E){
 	timespec temp;
@@ -54,8 +57,8 @@ void pop(){
 		else
 			break;
 }
-long double Heap_sort(){
-	clock_gettime(CLOCK_MONOTONIC, &S);
+void Heap_sort(){
+	top=0;
 	for(int i=0;i<n;i++){
 		push(a[i]);
 	}
@@ -63,12 +66,9 @@ long double Heap_sort(){
 		a[i] = heap[1];
 		pop();
 	}
-	clock_gettime(CLOCK_MONOTONIC, &E);
-	return diff(S, E);
 }
-long double Merge_sort(int l, int r){
-	clock_gettime(CLOCK_MONOTONIC, &S);
-	if(l==r) return 0;
+void Merge_sort(int l, int r){
+	if(l==r) return;
 	int m = (l+r)>>1;
 	Merge_sort(l, m);
 	Merge_sort(++m, r);
@@ -88,31 +88,18 @@ long double Merge_sort(int l, int r){
 		a[i] = q.front();
 		q.pop();
 	}
-	clock_gettime(CLOCK_MONOTONIC, &E);
-	return diff(S, E);
 }
-long double Quick_sort(int l, int r){
-	cout << l <<" "<< r<<"\n";
-	clock_gettime(CLOCK_MONOTONIC, &S);
-	if (r-l<=1) return 0;
+void Quick_sort(int l, int r){
+	if (r-l<1) return;
 	int ii, j;
-	for (ii=l-1, j=l;j<r;j++){
-		if(a[j]<a[r]){
-			cout << a[ii+1] << " ";
+	for (ii=l-1, j=l;j<r;j++)
+		if(a[j]<a[r])
 			swap(a[++ii], a[j]);
-			cout << ii << "\n";
-		}
-	}
 	swap(a[++ii], a[r]);
-	print();
-	cout << l << " " << ii << " " << r << "\n";
 	Quick_sort(l, ii-1);
 	Quick_sort(ii+1, r);
-	clock_gettime(CLOCK_MONOTONIC, &E);
-	return diff(S, E);
 }
-long double Shell_sort(){
-	clock_gettime(CLOCK_MONOTONIC, &S);
+void Shell_sort(){
 	vector<int> v;
 	for (int i=1, gap=1;gap<=n;i++){
 		v.push_back(gap);
@@ -123,34 +110,56 @@ long double Shell_sort(){
 	}
 	reverse(v.begin(), v.end());
 	for (int gap:v){
-		for (int i = gap;i<n;i++)
-			for (int j = i-gap;j>=0;j-=gap)
-				if (a[j]>a[j+gap])
-					swap(a[j], a[j+gap]);
+		for (int i = gap;i<n;i++){
+			int te = a[i];
+			int j;
+			for (j = i;j>=gap&&a[j - gap] > te;j-=gap)
+				a[j] = a[j - gap];
+			a[j] = te;
+		}
 	}
-	clock_gettime(CLOCK_MONOTONIC, &E);
-	return diff(S, E);
 }
 int main(){
-	for (int i=0;i<12;i++){
-		int *temp;
-		srand(time(NULL));
+	srand(time(NULL));
+	long double rt;
+	for (int i=0;i<13;i++){
 		n = s[i];
-		a = new int [n];
-		heap = new int [n];
-		temp = new int [n];
 		for (int j=0;j<n;j++)
-			temp[j] = rand();
+			temp[j] = ( rand() << 15 ) | rand();
 		cout << n << ":\n";
+		copy(temp, temp+n, t);
+		sort(t, t+n, cmp);
+		
 		copy(temp, temp+n, a);
-		print();
-		cout << fixed << setprecision(10) << Quick_sort(0, n-1) << test(temp)  << "\n";
+		clock_gettime(CLOCK_MONOTONIC, &S);
+		Heap_sort();
+		clock_gettime(CLOCK_MONOTONIC, &E);
+		rt = diff(S, E);
+		test();
+		cout << fixed << setprecision(10) << rt << test() << "\n";
+		
 		copy(temp, temp+n, a);
-		cout << fixed << setprecision(10) << Heap_sort() << test(temp) << "\n";
+		clock_gettime(CLOCK_MONOTONIC, &S);
+		Merge_sort(0, n-1);
+		clock_gettime(CLOCK_MONOTONIC, &E);
+		rt = diff(S, E);
+		test();
+		cout << fixed << setprecision(10) << rt << test() << "\n";
+		
 		copy(temp, temp+n, a);
-		cout << fixed << setprecision(10) << Merge_sort(0, n-1) << test(temp)  << "\n";
+		clock_gettime(CLOCK_MONOTONIC, &S);
+		Quick_sort(0, n-1);
+		clock_gettime(CLOCK_MONOTONIC, &E);
+		rt = diff(S, E);
+		test();
+		cout << fixed << setprecision(10) << rt << test() << "\n";
+		
 		copy(temp, temp+n, a);
-		cout << fixed << setprecision(10) << Shell_sort() << test(temp)  << "\n";
-		delete [] a, heap, temp;
+		clock_gettime(CLOCK_MONOTONIC, &S);
+		Shell_sort();
+		clock_gettime(CLOCK_MONOTONIC, &E);
+		rt = diff(S, E);
+		test();
+		cout << fixed << setprecision(10) << rt << test() << "\n";
 	}
 }
